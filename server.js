2ab195d152ed25ac2eb93e275f2a215e3c84df3a
Gabriel de Oliveira <gabriel-ao@hotmail.com>
2020-02-26 18:17:49 -0300

    conectando no banco mongodb

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const requireDir = require("require-dir");
 
+// iniciando o app
 const app = express();
 
+// iniciando o db
+mongoose.connect("mongodb://localhost:27017/nodeapi", {
+  useNewUrlParser: true
+});
+
+requireDir("./src/models");
+
+const Product = mongoose.model("Product");
+
+// primeira rota
 app.get("/", (req, res) => {
-  res.send("hello");
+  Product.create({
+    title: "react",
+    description: "testando processo",
+    url: "https://github.com/gabriel-ao/cursoNodeJs"
+  });
+
+  return res.send("hello");
 });
 
 app.listen(3001);
